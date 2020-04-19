e612f49437fdd2fb514f4e505ce67bdb1780eee4
Casuy <casuistry016@gmail.com>
2020-04-19 21:51:30 +1000

    json + combine

---
+++ SnakeApp/DataStore.swift
@@ -0,0 +1,24 @@
+//
+//  DataStore.swift
+//  SnakeApp
+//
+//  Created by Casuy on 19/4/20.
+//  Copyright © 2020 Casuy. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+class DataStore: ObservableObject {
+    @Published var posts: [Post] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        Api().getPosts { (posts) in
+            self.posts = posts
+        }
+    }
+}

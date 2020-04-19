9ba8da6b22b9fd7251071cd2d813ae2cdb0569ae
Casuy <casuistry016@gmail.com>
2020-04-19 20:54:48 +1000

    dismiss gesture

--- SnakeApp/SnakeList.swift
+++ SnakeApp/SnakeList.swift
@@ -12,10 +12,11 @@ struct SnakeList: View {
     @State var snakes = snakeData
     @State var active = false
     @State var activeIndex = -1
+    @State var activeView = CGSize.zero
     
     var body: some View {
         ZStack {
-            Color.black.opacity(active ? 0.5 : 0)
+            Color.black.opacity(Double(self.activeView.height / 500))
                 .animation(.linear)
                 .edgesIgnoringSafeArea(.all)
             
@@ -35,7 +36,8 @@ struct SnakeList: View {
                                 snake: self.snakes[index],
                                 active: self.$active,
                                 index: index,
-                                activeIndex: self.$activeIndex
+                                activeIndex: self.$activeIndex,
+                                activeView: self.$activeView
                             )
                                 .offset(y: self.snakes[index].show ? -geometry.frame(in: .global).minY : 0)
                                 .opacity(self.activeIndex != index && self.active ? 0 : 1)
@@ -68,6 +70,7 @@ struct SnakeView: View {
     @Binding var active: Bool
     var index: Int
     @Binding var activeIndex: Int
+    @Binding var activeView: CGSize
     
     
     
@@ -113,7 +116,7 @@ struct SnakeView: View {
                                 .foregroundColor(.white)
                         }
                         .frame(width: 36, height: 36)
-                        .background(Color.black)
+                        .background(Color.black.opacity(0.6))
                         .clipShape(Circle())
                         .opacity(show ? 1 : 0)
                     }
@@ -128,11 +131,28 @@ struct SnakeView: View {
             .padding(show ? 30 : 20)
             .padding(.top, show ? 30 : 0)
     //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
-                .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
-                .background(Color(snake.color))
+            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
+            .background(Color(snake.color))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                .shadow(color: Color(snake.color).opacity(0.3), radius: 20, x: 0, y: 20)
-            
+            .shadow(color: Color(snake.color).opacity(0.3), radius: 20, x: 0, y: 20)
+            .gesture(
+                show ?
+                DragGesture().onChanged { value in
+                    guard value.translation.height < 300 else { return }
+                    guard value.translation.height > 0 else { return }
+                    
+                    self.activeView = value.translation
+                }
+                .onEnded { value in
+                    if self.activeView.height > 50 {
+                        self.show = false
+                        self.active = false
+                        self.activeIndex = -1
+                    }
+                    self.activeView = .zero
+                }
+                : nil
+            )
             .onTapGesture {
                 self.show.toggle()
                 self.active.toggle()
@@ -145,7 +165,28 @@ struct SnakeView: View {
             
         }
         .frame(height: show ? screen.height : 280)
+        .scaleEffect(1 - self.activeView.height / 1000)
+        .rotation3DEffect(Angle(degrees: Double(self.activeView.height / 10)), axis: (x: 0, y: 10.0, z: 0))
+//        .hueRotation(Angle(degrees: Double(self.activeView.height)))
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .gesture(
+            show ?
+            DragGesture().onChanged { value in
+                guard value.translation.height < 300 else { return }
+                guard value.translation.height > 0 else { return }
+                
+                self.activeView = value.translation
+            }
+            .onEnded { value in
+                if self.activeView.height > 50 {
+                    self.show = false
+                    self.active = false
+                    self.activeIndex = -1
+                }
+                self.activeView = .zero
+            }
+            : nil
+        )
         .edgesIgnoringSafeArea(.all)
     }
 }
